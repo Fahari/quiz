fc5fd8ad91c4365096e863b76a27a03d1f563ec7
Fahari <karonjekevin67@gmail.com>
2018-12-10 22:08:05 +0300

    Redefined the whole JavaScript code.Did away completely with previous code while replacing with user input placevalue elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,10 @@
 $(document).ready(function() {
-  $("#form1").submit(function(event) {
+  $("#test").click(function() {
 
-    var username = $("#user").val()
-    var answers = ["C", "D", "C", "A", "B", "C", "A", "A", "A", "C"];
 
-    function checkedValue(radioName) {
-      var radios = document.getElementsByName(radioName);
-      for (var x = 0; x < radios.length; x++)
-        if (radios[x].checked) {
-          return radios[x].value;
-        }
-    }
-
-  >  function getScore() {
-      checkedValue();
-      var score = 0;
-      var totalQuestions = 10;
-      var response = checkedValue;
-      for (var i = 0; i < totalQuestions; i++)
-        if (response[i] === answers[i]) {
-          score += 1;
-          return score;
-        }
-
-    }
-
-    function returnScore() {
-      alert("Your score " + username + "is: " + getscore() + "/" + totalQuestions.length);
-    };
-
-    event.preventDefault();
-  });
-});
+    var answer=[];
+     answer.push ($("input:radio[name=question0]:checked").val());
+    answer.push ($("input:radio[name=question1]:checked").val());
+    answer.push( $("input:radio[name=question2]:checked").val());
+    answer.push($("input:radio[name=question3]:checked").val());
+    answer.push($("input:radio[name=question4]:checked").val());
