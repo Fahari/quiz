f6d932837ed6d7e3ca3d2d756f684648fb8a4492
Fahari <karonjekevin67@gmail.com>
2018-12-10 09:44:58 +0300

    add response and answers arrays to the JS file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var name = $("#user").val()
+var response = [];
+var answers = new Array(10);
+answers[0] = "B";
+answers[1] = "C";
+answers[2] = "A";
+answers[3] = "C";
+answers[4] = "D";
+answers[5] = "D";
+answers[6] = "D";
+answers[7] = "D";
+answers[8] = "C";
+answers[9] = "A";
