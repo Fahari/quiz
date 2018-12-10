839871e31689de71d115cb3c91267b8ea3206e72
Fahari <karonjekevin67@gmail.com>
2018-12-10 16:40:14 +0300

    add a delete of repetetive element entry in the array "answers"

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
-var name = $("#user").val()
-var response = [];
-var answers = new Array(10);
-answers[0] = "B";
-answers[1] = "C";
-answers[2] = "A";
-answers[3] = "C";
-answers[4] = "D";
-answers[5] = "D";
-answers[6] = "D";
-answers[7] = "D";
-answers[8] = "C";
-answers[9] = "A";
+$(document).ready(function() {
+  $("#form1").submit(function(event) {
+
+    var username = $("#user").val()
+    var answers = ["C", "D", "C", "A", "B", "C", "A", "A", "A", "C"];
+
+    function checkedValue(radioName) {
+      var radios = document.getElementsByName(radioName);
+      for (var x = 0; x < radios.length; x++)
+        if (radios[x].checked) {
+          return radios[x].value;
+        }
+    }
+
+  >  function getScore() {
+      checkedValue();
+      var score = 0;
+      var totalQuestions = 10;
+      var response = checkedValue;
+      for (var i = 0; i < totalQuestions; i++)
+        if (response[i] === answers[i]) {
+          score += 1;
+          return score;
+        }
+
+    }
+
+    function returnScore() {
+      alert("Your score " + username + "is: " + getscore() + "/" + totalQuestions.length);
+    };
+
+    event.preventDefault();
+  });
+});
