c352910223703fbfc59006a3ee9f85b6dc3ea528
Fahari <karonjekevin67@gmail.com>
2018-12-12 21:35:05 +0300

    add an update of UI interface and Business logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,26 @@ $(document).ready(function() {
     answer.push($("input:radio[name=question3]:checked").val());
     answer.push($("input:radio[name=question4]:checked").val());
 
-
-
-    var score = 0;
-
-
-    if (answer[0] == "C") {
-      score++;
-    }
-    if (answer[1] == "D") {
-      score++;
-    }
-    if (answer[2] == "C") {
-      score++;
-    }
-    if (answer[3] == "A") {
-      score++;
-    }
-    if (answer[4] == "D") {
-      score++;
-    }
     $("#form1").hide(function(event) {
       $("#display").html(userName + ", you scored " + score + "/5");
     });
   });
 });
+
+var score = 0;
+
+if (answer[0] == "C") {
+  score++;
+}
+if (answer[1] == "D") {
+  score++;
+}
+if (answer[2] == "C") {
+  score++;
+}
+if (answer[3] == "A") {
+  score++;
+}
+if (answer[4] == "D") {
+  score++;
+}
